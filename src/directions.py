3e5434e362d36bf1824f237db967790667e0014f
obub <bubenshchikov@iskrauraltel.ru>
2021-08-05 16:34:39 +0500

    fix directions

--- src/directions.py
+++ src/directions.py
@@ -1,16 +1,32 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 
 import src.constants as cnst
 
 
 class AbstractDirection(ABC):
-    pass
+
+    @abstractmethod
+    def turn_left(self):
+        pass
+
+    @abstractmethod
+    def turn_right(self):
+        pass
+
+    @abstractproperty
+    def current(self):
+        pass
 
 
 class BaseDirection(AbstractDirection):
 
     def __init__(self) -> None:
         self._directions = [cnst.NORTH, cnst.EAST, cnst.SOUTH, cnst.WEST]
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}:"
+                f"current direction is '{self.current}'; "
+                f"all are '{', '.join(self._directions)}'")
 
     @property
     def current(self) -> str:
